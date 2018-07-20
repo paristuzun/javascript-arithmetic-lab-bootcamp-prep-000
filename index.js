447ac9b19403aaf0a63cf62d5b8cdac8d65dc1f9
Paris Tuzun <paristuzun@gmail.com>
2018-07-20 23:17:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,59 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+// passed
+
+
+function inc(n) {
+  return n
+ 
+}
+
+
+function dec(n) {
+  return number
+}
+
+
+function makeInt(n) {
+  n = 2
+  parseInt('n')
+}
+
+function makeInt(n) {
+  parseInt('n', 10)
+}
+
+function makeInt(n) {
+  parseInt('nonsense!', 10)
+}
+
+function preserveDecimal(n) {
+  n = 2.222
+  parseFloat('n')
+}
+
+function preserveDecimal(n) {
+  parseFloat('n')
+}
+
+
+
+
+
+
+
+
